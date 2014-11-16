6bb9e44458f1d02059f7dafe29b67721d6858f07
olivierrr <oliviersinbox@gmail.com>
2014-11-15 21:08:56 -0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+
+;(function () {
+
+  var canvas = document.getElementById('c')
+    , ctx = canvas.getContext('2d')
+
+  var W = canvas.width = window.innerWidth
+    , H = canvas.height = window.innerHeight
+
+})()
