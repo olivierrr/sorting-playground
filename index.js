4fd837bfa760e1fb1943af7fc1f440365665c9ef
olivierrr <oliviersinbox@gmail.com>
2014-11-15 22:24:31 -0800

    drawing basics

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// % are always between 0.0 and 1.0
 ;(function () {
 
   var canvas = document.getElementById('c')
@@ -6,5 +7,49 @@
 
   var W = canvas.width = window.innerWidth
     , H = canvas.height = window.innerHeight
+
+  /**
+   * @param Width - overall width in pixels
+   * @param gutterP - % gutter width relative to bar width
+   * @param barCount - number of bars
+   *
+   * @note (barCount - 1) gutters are only between bars
+   */
+  function getSizes (Width, gutterP, barCount) {
+
+    var gutterWidth = Math.floor((Width * gutterP) / (barCount - 1))
+    var barWidth = Math.floor((Width * (1 - gutterP)) / barCount)
+    var padding = (Width - ((gutterWidth + barWidth) * barCount)) / 2
+
+    return {
+      gutterWidth: gutterWidth,
+      barWidth: barWidth,
+      paddingLeft: Math.floor(padding),
+      paddingRight: Math.ceil(padding),
+      barCount: barCount
+    }
+  }
+
+  /**
+   * @param sizes
+   *
+   * @todo remove H global dep
+   */
+  function drawBars (sizes) {
+
+    if(!sizes) return
+    else console.log(sizes)
+
+    for(var i=0; i<sizes.barCount; i++) {
+      ctx.fillRect(
+          sizes.paddingLeft + ((sizes.barWidth + sizes.gutterWidth) * i)
+        , H
+        , sizes.barWidth
+        , -H
+      )
+    }
+  }
+
+  drawBars(getSizes(W, 0.30, 80))
 
 })()
